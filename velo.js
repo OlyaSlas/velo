f539a0ebc9ec35ee7d8a60505a365914721ac74a
OlyaSlas <156464217+OlyaSlas@users.noreply.github.com>
2024-06-15 11:23:34 +0200

    Update velo.js

--- velo.js
+++ velo.js
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>Велосипед и Умный дом</title>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">//почему именно этот ксс
 <style>
 button {
     font-size: 24px;
